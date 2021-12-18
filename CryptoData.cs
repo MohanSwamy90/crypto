571248fe743af4d45b9fcf3eae6a8b4bb226a99b
MohanSwamy90 <96230468+MohanSwamy90@users.noreply.github.com>
2021-12-18 17:12:35 +0530

    cs files added

---
+++ CryptoData.cs
@@ -0,0 +1,78 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace Crypto
+{
+    public class CryptoData
+    {
+        [JsonProperty(PropertyName = "status")]
+        public Status status { get; set; }
+
+        [JsonProperty(PropertyName = "data")]
+        public List<Cryptdata> data { get; set; }
+    }
+
+    public class Status
+    {
+        [JsonProperty(PropertyName = "timestamp")]
+        public string timestamp { get; set; }
+
+        [JsonProperty(PropertyName = "error_code")]
+        public Int32 error_code { get; set; }
+
+        [JsonProperty(PropertyName = "elapsed")]
+        public Int32 elapsed { get; set; }
+
+        [JsonProperty(PropertyName = "credit_count")]
+        public Int32 credit_count { get; set; }
+
+        [JsonProperty(PropertyName = "total_count")]
+        public Int32 total_count { get; set; }
+    }
+}
+
+public class Cryptdata
+{
+    [JsonProperty(PropertyName = "id")]
+    public Int32 id { get; set; }
+
+    [JsonProperty(PropertyName = "name")]
+    public string name { get; set; }
+
+    [JsonProperty(PropertyName = "symbol")]
+    public string symbol { get; set; }
+
+    [JsonProperty(PropertyName = "slug")]
+    public string slug { get; set; }
+
+    [JsonProperty(PropertyName = "num_market_pairs")]
+    public Int32 num_market_pairs { get; set; }
+
+    [JsonProperty(PropertyName = "date_added")]
+    public string date_added { get; set; }
+
+    //[JsonProperty(PropertyName = "tags")]
+    //public List<string> tags { get; set; }
+    //[JsonProperty(PropertyName = "max_supply")]
+    //Int32? max_supply { get; set; }
+    [JsonProperty(PropertyName = "circulating_supply")]
+    public string circulating_supply { get; set; }
+
+    [JsonProperty(PropertyName = "total_supply")]
+    public string total_supply { get; set; }
+
+    [JsonProperty(PropertyName = "last_updated")]
+    public string last_updated { get; set; }
+
+    [JsonProperty(PropertyName = "platform")]
+    public Platform? platform { get; set; }
+
+    public class Platform
+    {
+        [JsonProperty(PropertyName = "name")]
+        public string name { get; set; }
+        [JsonProperty(PropertyName = "token_address")]
+        public string token_address { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using Newtonsoft.Json;
+using NSubstitute;
+using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Crypto
+{
+    internal class Program
+    {
+        private static string API_KEY = "e3738b41-b458-46c1-8d2e-4de15f09e11a";
+
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Starting timer with callback every 5 minutes.");
+
+                var startTimeSpan = TimeSpan.Zero;
+                var periodTimeSpan = TimeSpan.FromSeconds(60*5);
+
+                var timer = new System.Threading.Timer((e) =>
+                {
+                    displayCrypt();
+                    Console.WriteLine("-----------------'");
+                }, null, startTimeSpan, periodTimeSpan);
+
+                Console.WriteLine("Done. Press ENTER");
+
+                Console.ReadLine();
+                
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private static string makeAPICall()
+        {
+            var URL = new UriBuilder("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/new");
+
+            var queryString = HttpUtility.ParseQueryString(string.Empty);
+            queryString["start"] = "1";
+            queryString["limit"] = "5000";
+            queryString["convert"] = "USD";
+
+            URL.Query = queryString.ToString();
+
+            var client = new WebClient();
+            client.Headers.Add("X-CMC_PRO_API_KEY", API_KEY);
+            client.Headers.Add("Accepts", "application/json");
+            return client.DownloadString(URL.ToString());
+        }
+
+        private static void displayCrypt()
+        {
+            string result = makeAPICall();
+            Cryptdata objCryptData = new Cryptdata();
+            var jsonData1 = JsonConvert.DeserializeObject<CryptoData>(result);
+            var currentDate = DateTime.UtcNow.Date;
+            foreach (var crypt in jsonData1.data)
+            {
+                if (currentDate == DateTime.Parse(crypt.date_added).ToUniversalTime().Date)
+                {
+                    Console.WriteLine($"name: {crypt.name} - symbol: {crypt.symbol} - slug: {crypt.slug} - dateAdded: {crypt.date_added} - lastupdated: {crypt.last_updated} -platform-name: {crypt.platform.name} -platform-token: {crypt.platform.token_address}");
+                }
+            }
+        }
+
+        private static void callback(object state)
+        {
+            displayCrypt();
+        }
+    }
+}
